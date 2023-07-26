a9938fed7abaf474b09d22fa8c48478e8ca9b5c9
rancharaz <ranveer1990@hotmail.com>
2023-07-26 15:20:10 +0400

    initial commit

---
+++ app/crud-app/makeData.ts
@@ -0,0 +1,64 @@
+export type User = {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+  
+  };
+  
+  export const fakeData: User[] = [
+    {
+      id: '9s41rp',
+      firstName: 'David',
+      lastName: 'Beckham',
+      email: 'dbeckham@gmail.com'
+      
+    },
+    {
+      id: '08m6rx',
+      firstName: 'cristiano',
+      lastName: 'ronaldo',
+      email: 'cronaldo@hotmail.com'
+     
+    },
+    {
+      id: '5ymtrc',
+      firstName: 'thierry',
+      lastName: 'henry',
+      email: 'thenry@hotmail.com',
+ 
+    },
+    {
+      id: 'ek5b97',
+      firstName: 'George',
+      lastName: 'Best',
+      email: 'gbest0@yahoo.com'
+ 
+    },
+    {
+      id: 'xxtydd',
+      firstName: 'wayne',
+      lastName: 'rooney',
+      email: 'waynerooney@hotmail.com'
+    },
+    {
+      id: 'wzxj9m',
+      firstName: 'John',
+      lastName: 'lennon',
+      email: 'jlennon@gmail.com'
+    },
+    {
+      id: '21dwtz',
+      firstName: 'Bruce',
+      lastName: 'Wayne',
+      email: 'bwayne@yahoo.com'
+    },
+    {
+      id: 'o8oe4k',
+      firstName: 'Clark',
+      lastName: 'Kent',
+      email: 'clarkkent@gmail.com'
+    },
+  ];
+  
+ 
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import Example from './crud-app/CrudApp'
+export default function Home() {
+  return (
+   <>
+    <Example />
+   </>
+  )
+}
